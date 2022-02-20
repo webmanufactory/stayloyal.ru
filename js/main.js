2255b562dc7c8af548d5f7820476be66ec9c4a28
webmanufactory <bubunya@yandex.ru>
2022-02-20 22:58:02 +0300

    mob adaptation and more

--- js/main.js
+++ js/main.js
@@ -8,45 +8,66 @@ new Swiper('.catalogue__slider', {
 
 new Swiper('.popular-category__slider', {
 
-  slidesPerView: 3,
+  slidesPerView: 2,
   spaceBetween: 10,
-  watchOverflow: true,
-  setWrapperSize: true,
-  freeMode: false,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  centeredSlides: true,
+  breakpoints: {
+    475: {
+      slidesPerView: 3,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    },
+    320: {
+      slidesPerView: 1,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    },
+  }
 });
 
-let productGallerySlider1 = new Swiper(".product-gallery__slider-1", {
-  spaceBetween: 20,
-  slidesPerView: 3,
-  freeMode: true,
-  navigation: {
-    nextEl: ".product-gallery-button-next",
-    prevEl: ".product-gallery-button-prev",
-  },
-});
-let productGallerySlider2 = new Swiper(".product-gallery__slider-2", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: productGallerySlider1,
-  },
-});
+if (window.matchMedia("(max-width: 768px)").matches) {
+  let productGallerySlider2 = new Swiper(".product-gallery__slider-2", {
+    spaceBetween: 10,
+    pagination: {
+      el: '.product-gallery__slider-pagination',
+      clickable: true,
+    },
+  });
+}
+if (window.matchMedia("(min-width: 768px)").matches) {
+  let productGallerySlider1 = new Swiper(".product-gallery__slider-1", {
+    spaceBetween: 20,
+    slidesPerView: 3,
+    freeMode: true,
+    navigation: {
+      nextEl: ".product-gallery-button-next",
+      prevEl: ".product-gallery-button-prev",
+    },
+  });
+  let productGallerySlider2 = new Swiper(".product-gallery__slider-2", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: productGallerySlider1,
+    },
+  });
+}
 
+let tabNavs = document.querySelectorAll(".tab-trigger");
+let tabPanes = document.querySelectorAll(".tab-content");
+let mobMenu = document.querySelector('.mob-menu');
 
-var tabNavs = document.querySelectorAll(".tab-trigger");
-var tabPanes = document.querySelectorAll(".tab-content");
-
-for (var i = 0; i < tabNavs.length; i++) {
+for (let i = 0; i < tabNavs.length; i++) {
 
   tabNavs[i].addEventListener("click", function (e) {
     e.preventDefault();
-    var activeTabAttr = e.target.getAttribute("data-tab");
+    let activeTabAttr = e.target.getAttribute("data-tab");
 
-    for (var j = 0; j < tabNavs.length; j++) {
-      var contentAttr = tabPanes[j].getAttribute("data-tab-content");
+    for (let j = 0; j < tabNavs.length; j++) {
+      let contentAttr = tabPanes[j].getAttribute("data-tab-content");
 
       if (activeTabAttr === contentAttr) {
         tabNavs[j].classList.add("active");
