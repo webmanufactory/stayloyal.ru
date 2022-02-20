4fb2799f109c187dab79330f4f0444d4e7cdda30
webmanufactory <bubunya@yandex.ru>
2022-02-20 16:38:39 +0300

    final desktop version 1.0

--- js/main.js
+++ js/main.js
@@ -18,3 +18,43 @@ new Swiper('.popular-category__slider', {
     prevEl: ".swiper-button-prev",
   },
 });
+
+let productGallerySlider1 = new Swiper(".product-gallery__slider-1", {
+  spaceBetween: 20,
+  slidesPerView: 3,
+  freeMode: true,
+  navigation: {
+    nextEl: ".product-gallery-button-next",
+    prevEl: ".product-gallery-button-prev",
+  },
+});
+let productGallerySlider2 = new Swiper(".product-gallery__slider-2", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: productGallerySlider1,
+  },
+});
+
+
+var tabNavs = document.querySelectorAll(".tab-trigger");
+var tabPanes = document.querySelectorAll(".tab-content");
+
+for (var i = 0; i < tabNavs.length; i++) {
+
+  tabNavs[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    var activeTabAttr = e.target.getAttribute("data-tab");
+
+    for (var j = 0; j < tabNavs.length; j++) {
+      var contentAttr = tabPanes[j].getAttribute("data-tab-content");
+
+      if (activeTabAttr === contentAttr) {
+        tabNavs[j].classList.add("active");
+        tabPanes[j].classList.add("active");
+      } else {
+        tabNavs[j].classList.remove("active");
+        tabPanes[j].classList.remove("active");
+      }
+    };
+  });
+}
